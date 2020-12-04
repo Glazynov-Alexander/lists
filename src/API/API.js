1f504a8d3f90ca84fa4407f2ff67713496c788f3
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-12-04 16:02:15 +0300

    finished-auth-vk

--- src/API/API.js
+++ src/API/API.js
@@ -4,60 +4,64 @@ Axios.defaults.headers.common['Authorization'] = localStorage.getItem('user')
 if (localStorage.getItem('user') && localStorage.getItem('user').includes('Bearer') === false) {
     localStorage.removeItem("user")
 }
+let axios = Axios.create({
+    headers: {'Authorization': localStorage.getItem('user')},
+    baseURL: "https://backendtodos2.herokuapp.com"
+})
 
 //tasks
 export const deleteTaskAPI = (id) => {
-    return Axios.delete(`/lists/task/delete?id=${id}`)
+    return axios.delete(`/lists/task/delete?id=${id}`)
 }
 
 export const deleteTasksAPI = (symbol) => {
-    return Axios.delete(`/lists/tasks/deletes?symbol=${symbol}`)
+    return axios.delete(`/lists/tasks/deletes?symbol=${symbol}`)
 }
 
 export const createTaskAPI = (textTask, symbol) => {
-    return Axios.post("/lists/task/create", {taskChecked: false, textTask, symbol})
+    return axios.post("/lists/task/create", {taskChecked: false, textTask, symbol})
 }
 
 export const getTasksAPI = (symbol) => {
     Axios.defaults.headers.common['Authorization'] = localStorage.getItem('user')
-    return Axios.get(`/lists/get/tasks?symbol=${symbol}`)
+    return axios.get(`/lists/get/tasks?symbol=${symbol}`)
 }
 
 export const checkUpdateAPI = (id, checked) => {
-    return Axios.put("/lists/task/update", {id, checked})
+    return axios.put("/lists/task/update", {id, checked})
 }
 export const tasksCheckedAPI = (checked) => {
-    return Axios.put("/lists/tasks/updates", {checked})
+    return axios.put("/lists/tasks/updates", {checked})
 }
 
 
 //user
 export const createUserAPI = (name, password) => {
-    return Axios.post("/auth/registration", {name, password})
+    return axios.post("/auth/registration", {name, password})
 }
 export const getUserAPI = async (name, password, token) => {
     Axios.defaults.headers.common['Authorization'] = token
-    return Axios.get(`/auth/login`, {params: {user: token, name, password}})
+    return axios.get(`/auth/login`, {params: {user: token, name, password}})
 }
 
 export const othersGetUserAPI = (name, password, token) => {
-    return Axios.get(`/auth/login`, {params: {"name": name, "password": password}})
+    return axios.get(`/auth/login`, {params: {"name": name, "password": password}})
 }
 
 
 //tokens
 export const refreshTokensAPI = (ref) => {
-    return Axios.post(`/auth/refresh-tokens`, {refresh: ref})
+    return axios.post(`/auth/refresh-tokens`, {refresh: ref})
 }
 
 export const tokenAuthorization = (token) => {
     Axios.defaults.headers.common['Authorization'] = token
-    return Axios.get(`/auth/token-authorization`, {params: {user: token}})
+    return axios.get(`/auth/token-authorization`, {params: {user: token}})
 }
 
 
 Axios.interceptors.response.use(response => {
-            return response
+        return response
 
     }
     , async err => {
