53924a4b355df7a9f2c06d8aa76f51e5a01cb010
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-12-04 13:05:24 +0300

    finished-auth-vk

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //простой тест сервера
-
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
 
 //обслуживание html
 app.get('/*', function (req, res) {
