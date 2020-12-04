d5d6180f9234c7b7f8294115df5729e62781cb9b
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-12-04 17:01:53 +0300

    finished-auth-vk

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 // const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
@@ -6,7 +7,14 @@ const port = process.env.PORT || 8080;
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
 // app.use(favicon(__dirname + './build/favicon.png'));
+let corsOptions = {
+    origin: "https://gods123.herokuapp.com",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
+app.get("/products/:id", cors(corsOptions), function (req, res) {
+    res.json({msg: "This is CORS-enabled for only example.com."});
+});
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
